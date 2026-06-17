6697ffaccbc3a16cfece786417acb0f679f7d21a
Jhon <jhonatan.silvanunes@gmail.com>
2026-06-17 13:09:29 -0300

    Initial commit

---
+++ src/test/java/com/fcamara/fcamara_challenge/FcamaraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamara.fcamara_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcamaraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
